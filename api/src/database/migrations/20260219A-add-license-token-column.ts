469d648a6df64452537122ef5dc1179bd66dd084
andyvu97 <vuquanganh270297@gmail.com>
2026-02-11 09:23:03 +0700

    feat: add migration file to create license_token column for directus_settings table

---
+++ api/src/database/migrations/20260219A-add-license-token-column.ts
@@ -0,0 +1,13 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.alterTable('directus_settings', (table) => {
+		table.string('license_token', 255).nullable();
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.alterTable('directus_settings', (table) => {
+		table.dropColumn('license_token');
+	});
+}
