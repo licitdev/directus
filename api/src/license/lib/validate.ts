a75c17a0333e066460b2991171f9c2a9f664f40b
Dany <tuan.caoo8996@gmail.com>
2026-02-23 11:34:43 +0700

    rebase(license): resolve conflicts

--- api/src/license/lib/validate.ts
+++ api/src/license/lib/validate.ts
@@ -25,8 +25,6 @@ export async function validate({
 	const baseUrl = url.replace(/\/$/, '');
 	const verifyUrl = `${baseUrl}/v1/validate`;
 
-	let token: string;
-
 	try {
 		const getTokenResponse = await axios.post<ValidateLicenseResponse>(verifyUrl, {
 			license_key,
@@ -34,13 +32,12 @@ export async function validate({
 			public_url,
 		});
 
-		const { token: responseToken } = getTokenResponse.data;
+		const { token } = getTokenResponse.data;
 
 		if (typeof token !== 'string' || !token) {
-			throw new Error('Missing or invalid license token.');
+			throw new InvalidLicenseTokenError();
 		}
 
-		await setLicenseCaches(token);
 		return { token };
 	} catch (error) {
 		if (isDirectusError(error)) throw error;
@@ -64,7 +61,4 @@ export async function validate({
 
 		throw new InvalidLicenseKeyError({ reason });
 	}
-
-	await setLicenseCaches(token);
-	return { token };
 }
